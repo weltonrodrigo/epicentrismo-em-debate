ddf34011185f0587cc8d39dafe223a2505d54e57
Welton Rodrigo Torres Nascimento <rodrigo@familianascimento.org>
2023-04-03 09:30:24 -0300

    novo script de sync

--- bin/last.bash
+++ bin/last.bash
@@ -1,25 +1,18 @@
-#!/usr/bin/env bash
-#
-
+#!/bin/bash
 
 function last {
     url=$1
-    titles=$(curl -s $url | ack title | head -3 | tail -1)
-    echo "$titles"
-}
-
-function clean {
-    raw_title=$1
-    echo "$(echo "$1" | sed -e 's/.*\[\(.*\)\]\].*/\1/')"
+    title=$(curl -s $url | ack -oP '(?<=<title><!\[CDATA\[).*(?=\]\]></title>)' | sed -n 2p)
+    echo "$title"
 }
 
 function videos {
     url=$1
-    data=$(youtube-dl --dump-json "$url" --playlist-items 2 | jq '"\(.title) \(.id)"')
+    data=$(youtube-dl --dump-json --dateafter now-7day --playlist-end 2 "$url" | jq '. | "\(.title) \(.id)"' |tac )
     echo "$data"
 }
 
-declare -a  urls=(
+declare -a urls=(
     https://anchor.fm/s/7f9e6388/podcast/rss
     https://anchor.fm/s/2d664658/podcast/rss
     https://anchor.fm/s/8470a768/podcast/rss
@@ -31,14 +24,41 @@ declare -a playlists=(
     'https://www.youtube.com/playlist?list=PLoXdlLuaGN8REjJ-bjDenWVaUIRhqWyvt'
 )
 
-for i in "${urls[@]}"
-do 
-    out=$(clean "$(last $i)")
-    echo ANCHOR $out
-done
+declare -a podcast_names=(
+    'epicentrismo'
+    'tertuliamatinal'
+    'circulo'
+)
 
-for i in "${playlists[@]}"
-do 
-    out=$(videos $i)
-    echo $out
+for i in "${!urls[@]}"; do
+    podcast_title=$(last "${urls[$i]}")
+    video_data=$(videos "${playlists[$i]}")
+    IFS=$'\n' read -d '' -a video_list <<< "${video_data}"
+
+    echo $video_data
+
+    found=0
+    oldest_video=""
+    for video in "${video_list[@]}"; do
+        video_id=$(echo $video | awk '{print $NF}')
+        video_title=$(echo $video | awk '{$NF=""; print $0}')
+
+        # echo $video $video_id $video_title
+
+        if [ "$podcast_title" == "$video_title" ]; then
+            found=1
+            echo "Match: $podcast_title == $video_title"
+            break
+        else
+            if [ -z "$oldest_video" ]; then
+                oldest_video=$video
+            fi
+        fi
+    done
+
+    if [ "$found" -eq 0 ]; then
+        echo "Mismatch: $podcast_title $video_title"
+        echo "{\"id\": \"$(echo ${oldest_video} | awk '{print $NF}')}" > "${podcast_names[$i]}.json"
+    fi
+
 done
